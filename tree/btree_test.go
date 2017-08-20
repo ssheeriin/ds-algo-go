2d7db15bf37e69421f223109c2b2d4437a6dfcd2
sherin stephen <ssheeriin@github>
2017-08-20 19:40:54 +0530

    test for add

--- tree/btree_test.go
+++ tree/btree_test.go
@@ -1,6 +1,11 @@
 package tree
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
 
 func TestBSTree_Add(t *testing.T) {
 	bst := new(BSTree)
@@ -11,6 +16,39 @@ func TestBSTree_Add(t *testing.T) {
 	bst.Add(9)
 	bst.Add(2)
 	bst.Add(6)
+	bst.Add(4)
+	bst.Add(8)
 
-	bst.InOrder()
+	traverse := bst.InOrder
+	out := executeFuncAndCaptureOut(traverse)
+
+	expected := "123456789"
+	if expected != out {
+		t.Errorf("expected: %s, actual:%s", expected, out)
+	}
+}
+
+type traverse func()
+
+func executeFuncAndCaptureOut(t traverse) string {
+	// capture std out
+	old := os.Stdout
+	// keep backup of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	// do printing
+	t()
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+	// back to normal state
+	w.Close()
+	os.Stdout = old
+	// restoring the real stdout
+	out := <-outC
+	return out
 }
